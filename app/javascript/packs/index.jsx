db1058cfb4398673822d2d0c0872cc258313c7ef
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-17 18:28:22 -0300

    add global styles

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
-document.addEventListener('turbolinks:load', function () {
+import './assets/styles/global.css';
+
+document.addEventListener('turbolinks:load', () => {
   ReactDOM.render(<App />, document.getElementById('root'));
 });
