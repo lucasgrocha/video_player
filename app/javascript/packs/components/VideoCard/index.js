3c7293ca2dbf0f8dfc098dfb0a0ce04d9e15e702
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-18 14:38:28 -0300

    add videoCard component

--- app/javascript/packs/components/VideoCard/index.js
+++ app/javascript/packs/components/VideoCard/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import './styles.css';
 
-function VideoCard() {
-  return <div />;
+function VideoCard(props) {
+  return (
+    <div className="video-card">
+      <div className="thumbnail">
+        <img src={props.thumbnail_url} alt="Thumb" />
+      </div>
+
+      <div className="info">
+        <h3 id="video-name">{props.name}</h3>
+        <div id="user-name">{props.userName}</div>
+        <div id="views">{props.views} visualizações</div>
+      </div>
+    </div>
+  );
 }
+
+VideoCard.propTypes = {
+  thumbnail_url: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  views: PropTypes.number.isRequired,
+  userName: PropTypes.string.isRequired,
+};
 
 export default VideoCard;
--- app/javascript/packs/pages/Home/index.js
+++ app/javascript/packs/pages/Home/index.js
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import VideoCard from '../../components/VideoCard';
 
-import videosService from '../../services/videosService'
+import videosService from '../../services/videosService';
+
+import './styles.css';
 
 function Home() {
-
-  const [videos, setVideos] = useState()
+  const [videos, setVideos] = useState();
 
   useEffect(() => {
-    videosService.index().then(res => {
-      setVideos(res.data)
-    })
-  }, [])
+    videosService.index().then((res) => {
+      setVideos(res.data);
+    });
+  }, []);
 
   if (!videos) {
-    return null
+    return null;
   }
 
   return (
-    <div className='container'>
-      { videos[1].name }
+    <div className="container">
+      <div id="videos">
+        {videos.map((video) => (
+          <VideoCard
+            key={video.id}
+            name={video.name}
+            thumbnail_url={video.thumbnail_url}
+            views={video.views}
+            userName={video.user_name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
