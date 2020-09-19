4eaefac28d8841e6a8992d17a9967f5ea9dd5aa6
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-19 11:29:18 -0300

    add navbar component

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
 import VideoShow from './pages/VideoShow';
@@ -8,6 +9,8 @@ import VideoShow from './pages/VideoShow';
 function AppRoutes() {
   return (
     <BrowserRouter>
+    <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/videos/:id" element={<VideoShow />} />
