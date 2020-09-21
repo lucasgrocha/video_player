3b2d176d79bedf66bd19313aedce6a38083c972e
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-21 10:28:44 -0300

    add factory bot rails to rspec

--- spec/requests/video_request_spec.rb
+++ spec/requests/video_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'Videos', type: :request do
+RSpec.describe 'Api::V1::VideosController', type: :request do
   it 'returns true' do
-    expect(true).to be_truthy
+    expect(create(:user)).to be_truthy
   end
 end
